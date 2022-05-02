83c491b4b9e9062262e055c8701b1a9161551c00
gpsub <giriprasad590@gmail.com>
2022-05-02 21:49:41 +0400

    changes

--- hospital.py
+++ hospital.py
@@ -33,53 +33,53 @@ hospital.add_node(15,x=200,y=400)
 hospital.add_node(16,x=250,y=400)
 
 
-hospital.add_edge(1,2,capacity=100)
-hospital.add_edge(1,3,capacity=80)
-hospital.add_edge(4,2,capacity=80)
-hospital.add_edge(4,3,capacity=100)
+hospital.add_edge(1,2,capacity=700)
+hospital.add_edge(1,3,capacity=700)
+hospital.add_edge(4,2,capacity=700)
+hospital.add_edge(4,3,capacity=700)
 
 # exit1
-hospital.add_edge(2,7,capacity=100)
-hospital.add_edge(2,6,capacity=100)
+hospital.add_edge(2,7,capacity=1200)
+hospital.add_edge(2,6,capacity=1200)
 
-hospital.add_edge(3,7,capacity=100)
-hospital.add_edge(3,6,capacity=100)
+hospital.add_edge(3,7,capacity=1200)
+hospital.add_edge(3,6,capacity=1200)
 
 
-hospital.add_edge(5,6,capacity=220)
-hospital.add_edge(5,7,capacity=200)
-hospital.add_edge(8,6,capacity=220)
-hospital.add_edge(8,7,capacity=200)
+hospital.add_edge(5,6,capacity=800)
+hospital.add_edge(5,7,capacity=800)
+hospital.add_edge(8,6,capacity=800)
+hospital.add_edge(8,7,capacity=800)
 
 #exit2
-hospital.add_edge(6,10,capacity=100)
-hospital.add_edge(6,11,capacity=100)
+hospital.add_edge(6,10,capacity=2800)
+hospital.add_edge(6,11,capacity=2800)
 
-hospital.add_edge(7,10,capacity=200)
-hospital.add_edge(7,11,capacity=200)
+hospital.add_edge(7,10,capacity=2800)
+hospital.add_edge(7,11,capacity=2800)
 
 
-hospital.add_edge(9,10,capacity=350)
-hospital.add_edge(9,11,capacity=300)
-hospital.add_edge(12,10,capacity=350)
-hospital.add_edge(12,11,capacity=300)
+hospital.add_edge(9,10,capacity=800)
+hospital.add_edge(9,11,capacity=800)
+hospital.add_edge(12,10,capacity=800)
+hospital.add_edge(12,11,capacity=800)
 
 #exit3
 
-hospital.add_edge(10,14,capacity=100)
-hospital.add_edge(11,15,capacity=100)
+hospital.add_edge(10,14,capacity=4200)
+hospital.add_edge(11,15,capacity=4200)
 
-hospital.add_edge(10,15,capacity=200)
-hospital.add_edge(11,14,capacity=200)
+hospital.add_edge(10,15,capacity=4200)
+hospital.add_edge(11,14,capacity=4200)
 
-hospital.add_edge(13,14,capacity=500)
-hospital.add_edge(13,15,capacity=550)
-hospital.add_edge(16,15,capacity=550)
-hospital.add_edge(16,14,capacity=500)
+hospital.add_edge(13,14,capacity=1000)
+hospital.add_edge(13,15,capacity=1000)
+hospital.add_edge(16,15,capacity=1000)
+hospital.add_edge(16,14,capacity=1000)
 
 #finalexit
-hospital.add_edge(14,0,capacity=2000)
-hospital.add_edge(15,0,capacity=2000)
+hospital.add_edge(14,0,capacity=10000)
+hospital.add_edge(15,0,capacity=10000)
 ## super source vertex
 hospital.add_edge(20,1,capacity=floor_weight_list[0][0])
 hospital.add_edge(20,4,capacity=floor_weight_list[0][1])
@@ -183,6 +183,17 @@ output.add_edge(16,14,weight=int(flow_dict[16][14]))
 output.add_edge(14,0,weight=int(flow_dict[14][0]))
 output.add_edge(15,0,weight=int(flow_dict[15][0]))
 
+# output.add_edge(20,1,weight=flow_dict[20][1])
+# output.add_edge(20,4,weight=flow_dict[20][4])
+
+# output.add_edge(20,5,weight=flow_dict[20][5])
+# output.add_edge(20,8,weight=flow_dict[20][8])
+
+# output.add_edge(20,9,weight=flow_dict[20][9])
+# output.add_edge(20,12,weight=flow_dict[20][12])
+
+# output.add_edge(20,13,weight=flow_dict[20][13])
+# output.add_edge(20,16,weight=flow_dict[20][16])
 
 
 
