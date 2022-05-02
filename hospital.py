1dadea55b7e7e2e46d84867ae2ef0aa646fa2bab
gpsub <giriprasad590@gmail.com>
2022-05-02 20:27:41 +0400

    added hospital network

--- hospital.py
+++ hospital.py
@@ -4,23 +4,86 @@ import networkx as nx
 from pyvis.network import Network
 hospital = nx.DiGraph()
 
-
-hospital.add_node(0,label='exit',x=200,y=300)
+hospital.add_node(0,label='exit',x=175,y=500)
+#floor3
 hospital.add_node(1,x=100,y=100)
-hospital.add_node(2,x=125,y=100)
-hospital.add_node(3,x=150,y=100)
-hospital.add_node(4,x=175,y=100)
+hospital.add_node(2,x=150,y=100)
+hospital.add_node(3,x=200,y=100)
+hospital.add_node(4,x=250,y=100)
+#floor2
 hospital.add_node(5,x=100,y=200)
-hospital.add_node(6,x=125,y=200)
-hospital.add_node(7,x=150,y=200)
-hospital.add_node(8,x=175,y=200)
+hospital.add_node(6,x=150,y=200)
+hospital.add_node(7,x=200,y=200)
+hospital.add_node(8,x=250,y=200)
+#floor1
+hospital.add_node(9,x=100,y=300)
+hospital.add_node(10,x=150,y=300)
+hospital.add_node(11,x=200,y=300)
+hospital.add_node(12,x=250,y=300)
+#floor0
+hospital.add_node(13,x=100,y=400)
+hospital.add_node(14,x=150,y=400)
+hospital.add_node(15,x=200,y=400)
+hospital.add_node(16,x=250,y=400)
 
 
-corridors=[(1,2),(2,3),(3,4),(4,8),(8,7),(7,6),(6,5),(5,1),(6,0),(7,0)]
+hospital.add_edge(1,2,capacity=100)
+hospital.add_edge(1,3,capacity=80)
+hospital.add_edge(4,2,capacity=80)
+hospital.add_edge(4,3,capacity=100)
 
-hospital.add_edges_from(corridors)
+# exit1
+hospital.add_edge(2,7,capacity=100)
+hospital.add_edge(2,6,capacity=100)
 
-nt = Network('600px','600px')
+hospital.add_edge(3,7,capacity=100)
+hospital.add_edge(3,6,capacity=100)
+
+
+hospital.add_edge(5,6,capacity=220)
+hospital.add_edge(5,7,capacity=200)
+hospital.add_edge(8,6,capacity=220)
+hospital.add_edge(8,7,capacity=200)
+
+#exit2
+hospital.add_edge(6,10,capacity=100)
+hospital.add_edge(6,11,capacity=100)
+
+hospital.add_edge(7,10,capacity=200)
+hospital.add_edge(7,11,capacity=200)
+
+
+
+hospital.add_edge(9,10,capacity=350)
+hospital.add_edge(9,11,capacity=300)
+hospital.add_edge(12,10,capacity=350)
+hospital.add_edge(12,11,capacity=300)
+
+#exit3
+
+hospital.add_edge(10,14,capacity=100)
+hospital.add_edge(11,15,capacity=100)
+
+hospital.add_edge(10,15,capacity=200)
+hospital.add_edge(11,14,capacity=200)
+
+
+
+
+hospital.add_edge(13,14,capacity=500)
+hospital.add_edge(13,15,capacity=550)
+hospital.add_edge(16,15,capacity=550)
+hospital.add_edge(16,14,capacity=500)
+
+#finalexit
+hospital.add_edge(14,0,capacity=200)
+hospital.add_edge(15,0,capacity=200)
+
+
+
+
+
+nt = Network('800px','800px')
 nt.from_nx(hospital)
 nt.toggle_drag_nodes(True)
 nt.toggle_physics(False)
