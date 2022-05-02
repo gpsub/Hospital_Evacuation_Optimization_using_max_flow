7a2846814ca0f368881fd4661e1e792fcec7642e
gpsub <giriprasad590@gmail.com>
2022-05-02 18:43:52 +0400

    hospital network added

--- hospital.py
+++ hospital.py
@@ -1,14 +1,19 @@
 ## Graph visualization
+from cv2 import HOGDESCRIPTOR_DEFAULT_NLEVELS
 import networkx as nx
 from pyvis.network import Network
 hospital = nx.DiGraph()
 
-## rooms are 1 -2 -3 -4
-#           |        |
-#           5- 6-7 - 8
-#               |           
-#               0=exit
-#
+
+hospital.add_node(0,label='exit',x=200,y=300)
+hospital.add_node(1,x=100,y=100)
+hospital.add_node(2,x=125,y=100)
+hospital.add_node(3,x=150,y=100)
+hospital.add_node(4,x=175,y=100)
+hospital.add_node(5,x=100,y=200)
+hospital.add_node(6,x=125,y=200)
+hospital.add_node(7,x=150,y=200)
+hospital.add_node(8,x=175,y=200)
 
 
 corridors=[(1,2),(2,3),(3,4),(4,8),(8,7),(7,6),(6,5),(5,1),(6,0),(7,0)]
@@ -17,4 +22,6 @@ hospital.add_edges_from(corridors)
 
 nt = Network('600px','600px')
 nt.from_nx(hospital)
+nt.toggle_drag_nodes(True)
+nt.toggle_physics(False)
 nt.show("hospital.html")
